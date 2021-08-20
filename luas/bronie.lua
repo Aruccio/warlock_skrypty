8510858ced518fc4c1be29056bea0b165300f0b1
Aruccio <arleni@o2.pl>
2021-08-20 17:53:26 +0200

    1.6 szermierz
    szermierz ma teraz 4 kwadraciki, to osobny folder szermierz-skrypty

---
+++ luas/bronie.lua
@@ -0,0 +1,15 @@
+function KutaDziwerowanaSzabla(calosc)
+
+setFgColor(128, 255, 255)
+
+    echo(calosc)
+    resetFormat()
+  
+end
+
+function ZwyklaBron(calosc)
+setFgColor(128, 255, 255)
+
+    echo(calosc)
+    resetFormat()
+end
--- luas/pojemniki.lua
+++ luas/pojemniki.lua
@@ -1,38 +1,53 @@
 function ItemsInRows(pojemnik,podzielone)
 
-setFgColor(255, 255, 179)
-echo("\n"..pojemnik.. " zawiera:\n")
-resetFormat()
-echo("/=======================================\\\n")
+  setFgColor(255, 255, 179)
+  echo("\n"..pojemnik.. " zawiera:\n")
+  resetFormat()
+
+--  if pojemnik=='.*woreczek.*' then
+--    HerbsInRown(pojemnik,podzielone)
+--  elseif
+
+  echo("/=================================================\\\n")
 
 
-for index,data in ipairs(podzielone) do
-    echo("|  ")
-    if string.find(data,'miedzian.* ') then
-    setFgColor(255, 153, 51)
-    elseif string.find(data,'srebrn.* ') then
-    setFgColor(255, 255, 255)
-    elseif string.find(data,' zlot.*') then
-    setFgColor(255, 230, 128)
-    elseif string.find(data,' platynow.*') then
-    setFgColor(179, 255, 255)
-   elseif string.find(data,' (szabl|miecz|topor).*') then
-    setFgColor(128, 255, 170)
-  end
-  
-   
-    echo(data)
-    resetFormat()
-    local temp = "|  "..data
-    local length = 40-temp:len()
-    local toprint =""
-    for i = 1,length,1 do
-      toprint = toprint.." "
+    for index,data in ipairs(podzielone) do
+      echo("|  ")
+      if string.find(data,'miedzian.* ') then
+        setFgColor(255, 153, 51)
+      elseif string.find(data,'srebrn.* ') then
+        setFgColor(255, 255, 255)
+      elseif string.find(data,'zlot.*') then
+        setFgColor(255, 230, 128)
+      elseif string.find(data,' platynow.*') then
+        setFgColor(179, 255, 255)
+      elseif string.find(data,' (szabl|miecz|topor).*') then
+        setFgColor(128, 255, 170)
+      end
+
+      echo(data)
+      resetFormat()
+      local temp = "|  "..data
+      local length = 50-temp:len()
+      local toprint =""
+      for i = 1,length,1 do
+        toprint = toprint.." "
+      end
+      echo(toprint.."|\n")
     end
-    echo(toprint.."|\n")
+    echo("\\=================================================/\n")
+--  end
 end
-echo("\\=======================================/\n")
+
+function HerbsInRown(pojemnik,podzielone)
 
 end
 
+
+
+--function create_regexp_filter(tab, endline)
+--    return function(item)
+--        return rex.find(item.name, create_pattern(tab, endline))
+--    end
+--end
 
